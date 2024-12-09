c573f45d7f5e02bd6401a6538ae90f2c690a1d60
paul-talma <109061871+paul-talma@users.noreply.github.com>
2024-12-08 19:33:33 -0800

    finish day 6

--- day6/aoc6.py
+++ day6/aoc6.py
@@ -1,113 +1,97 @@
 from copy import deepcopy
 
+from tqdm import trange
+
 
 def get_input(path):
     with open(path, "r") as file:
-        grid = [list(line) for line in file.readlines()]
+        grid = [list(line.strip()) for line in file.readlines()]
         return grid
 
 
-class Board:
-    def __init__(self, grid):
-        self.grid = grid
-        self.next_dirs = {
-            (0, -1): (-1, 0),
-            (-1, 0): (0, 1),
-            (0, 1): (1, 0),
-            (1, 0): (0, -1),
-        }
-        self.guard_dir = (-1, 0)
-        self.guard_pos = self.get_guard_pos()
-        self.unique_visits = 1
+class Guard:
+    def __init__(self, map):
+        self.grid = map
         self.trajectory = set()
+        self.positions = set()
+        self.dir = "N"
+        self.mov = {"N": (-1, 0), "S": (1, 0), "W": (0, -1), "E": (0, 1)}
+        self.rotate = {"N": "E", "E": "S", "S": "W", "W": "N"}
+        self.pos = self._get_pos()
+        self.escaped = False
 
-    def get_guard_direction(self):
-        for dir in self.directions.keys():
-            if dir in self.grid:
-                return self.directions[dir]
+    def _get_pos(self):
+        for row_id, row in enumerate(self.grid):
+            for col_id, col in enumerate(row):
+                if col == "^":
+                    self.grid[row_id][col_id] = "."
+                    # self.trajectory.add(((row_id, col_id), "N"))
+                    # self.positions.add((row_id, col_id))
+                    return row_id, col_id
+        raise ValueError("Guard not found")
 
-    def get_guard_pos(self):
-        for x, row in enumerate(self.grid):
-            for y, elem in enumerate(row):
-                if elem == "^":
-                    return x, y
-        raise ValueError("guard not found")
+    def _update_pos(self, dir):
+        new_pos = self.pos[0] + dir[0], self.pos[1] + dir[1]
+        return new_pos
 
-    def next_pos(self):
-        return (
-            self.guard_pos[0] + self.guard_dir[0],
-            self.guard_pos[1] + self.guard_dir[1],
-        )
-
-    def in_bounds(self, x, y):
+    def _in_bounds(self, pos):
+        x, y = pos
         return 0 <= x < len(self.grid) and 0 <= y < len(self.grid[0])
 
-    def check_guard_front(self):
-        x, y = self.next_pos()
-        if self.in_bounds(x, y):
+    def _look_ahead(self):
+        x, y = self._update_pos(self.mov[self.dir])
+        if self._in_bounds((x, y)):
             return self.grid[x][y]
         return None
 
-    def rotate_guard(self):
-        self.guard_dir = self.next_dirs[self.guard_dir]
+    def _rotate(self):
+        self.dir = self.rotate[self.dir]
 
-    def mark_guard_pos(self):
-        x, y = self.guard_pos
-        self.grid[x][y] = "X"
+    def _move(self):
+        x, y = self.mov[self.dir]
+        self.pos = self.pos[0] + x, self.pos[1] + y
 
-    def move_guard(self):
-        x = self.guard_pos[0] + self.guard_dir[0]
-        y = self.guard_pos[1] + self.guard_dir[1]
-        self.guard_pos = x, y
+    def _time_step(self):
+        ahead = self._look_ahead()
+        if not ahead:
+            self.escaped = True
+            return
+        if ahead == "#":
+            self._rotate()
+            return
+        self._move()
 
-    def step_guard(self):
-        next_item = self.check_guard_front()
-        if not next_item:
-            return False
-        if next_item == "#":
-            self.rotate_guard()
-            return True
-        if next_item == ".":
-            self.unique_visits += 1
-        self.mark_guard_pos()
-        self.move_guard()
+    def escapes(self):
+        while not self.escaped:
+            self.trajectory.add((self.pos, self.dir))
+            self.positions.add(self.pos)
+            self._time_step()
+            if (self.pos, self.dir) in self.trajectory and not self.escaped:
+                return False
         return True
 
-    def walk_guard(self):
-        while self.step_guard():
-            pass
-
-    def get_grid(self):
-        return self.grid
-
-    def has_loop(self):
-        while True:
-            self.trajectory.add((self.guard_pos, self.guard_dir))
-            end = not self.step_guard()
-            if (self.guard_pos, self.guard_dir) in self.trajectory:
-                return True
-            if end:
-                return False
+    def num_visited_states(self):
+        return len(self.positions)
 
 
 def phase_2():
-    traps = 0
-    for x in range(len(grid)):
+    obstructions = 0
+    for x in trange(len(grid)):
         for y in range(len(grid[0])):
             if grid[x][y] == ".":
-                new_grid = deepcopy(initial_grid)
+                new_grid = deepcopy(grid)
                 new_grid[x][y] = "#"
-                new_board = Board(new_grid)
-                if new_board.has_loop():
-                    traps += 1
-    return traps
+                new_guard = Guard(new_grid)
+                if not new_guard.escapes():
+                    obstructions += 1
+
+    return obstructions
 
 
 if __name__ == "__main__":
-    initial_grid = get_input("input.txt")
-    grid = deepcopy(initial_grid)
-    board = Board(grid)
-    board.walk_guard()
-    print(f"Phase 1: {board.unique_visits}")
-    num_traps = phase_2()
-    print(f"Phase 2: {num_traps}")
+    grid = get_input("input.txt")
+    phase_1_grid = deepcopy(grid)
+    guard = Guard(phase_1_grid)
+    print(guard.escapes())
+    print(guard.num_visited_states())
+    print(phase_2())
